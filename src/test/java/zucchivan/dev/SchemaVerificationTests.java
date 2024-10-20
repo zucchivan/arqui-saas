b686c71cff4ac1e29dbcc091dca4bec1c380fe45
Ivan <zucchivan@gmail.com>
2024-10-20 16:05:08 +0100

    introduce initial db schema and schema verification tests

---
+++ src/test/java/zucchivan/dev/SchemaVerificationTests.java
@@ -0,0 +1,30 @@
+package zucchivan.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class SchemaVerificationTests {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public SchemaVerificationTests(@Autowired JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Test
+    public void testTablesExist() {
+        Integer userTableCount = jdbcTemplate.queryForObject(
+                "SELECT COUNT(*) FROM information_schema.tables WHERE table_name = 'USER'", Integer.class);
+        Integer projectTableCount = jdbcTemplate.queryForObject(
+                "SELECT COUNT(*) FROM information_schema.tables WHERE table_name = 'PROJECT'", Integer.class);
+
+        assertThat(userTableCount).isEqualTo(1);
+        assertThat(projectTableCount).isEqualTo(1);
+    }
+
+}
