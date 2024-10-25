98716618d4f165d26a98c18505e7df15c666c111
Ivan <zucchivan@gmail.com>
2024-10-25 14:06:32 +0100

    adjust user service to use repository

--- src/main/java/zucchivan/dev/service/UserService.java
+++ src/main/java/zucchivan/dev/service/UserService.java
@@ -2,13 +2,20 @@ package zucchivan.dev.service;
 
 import org.springframework.stereotype.Service;
 import zucchivan.dev.data.AppUser;
+import zucchivan.dev.data.repository.AppUserRepository;
 
 import java.util.List;
 
 @Service
 public class UserService {
 
+    private final AppUserRepository appUserRepository;
+
+    public UserService(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+
     public List<AppUser> fetchAllUsers() {
-        return null; //TODO
+        return appUserRepository.findAll();
     }
 }
